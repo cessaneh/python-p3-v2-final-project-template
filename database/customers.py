a7e64827a73cd98334c0a4da4dbcceb11315249a
princess <princessmumbi001@gmail.com>
2024-06-21 14:27:18 +0300

    customer.py

--- database/customers.py
+++ database/customers.py
@@ -1,19 +1,19 @@
 
-# import sqlite3
+import sqlite3
 
-# class CustomerDB:
-#     #access database
-#     def _init_(self):
-#         self.connection = sqlite3.connect("online_store.db")
-#         self.cursor = self.connection.cursor()
-# #add sth
-#     def add_customer(self, name, email):
-#         self.cursor.execute("INSERT INTO customers (name, email) VALUES (?, ?)", (name, email))
-#         self.connection.commit()
-# #returns all customers
-#     def get_all_customers(self):
-#         self.cursor.execute("SELECT * FROM customers")
-#         return self.cursor.fetchall()
-# #close db
-#     def close(self):
-#         self.connection.close()
+class CustomerDB:
+    #access database
+    def _init_(self):
+        self.connection = sqlite3.connect("online_store.db")
+        self.cursor = self.connection.cursor()
+#add sth
+    def add_customer(self, name, email):
+        self.cursor.execute("INSERT INTO customers (name, email) VALUES (?, ?)", (name, email))
+        self.connection.commit()
+#returns all customers
+    def get_all_customers(self):
+        self.cursor.execute("SELECT * FROM customers")
+        return self.cursor.fetchall()
+#close db
+    def close(self):
+        self.connection.close()
